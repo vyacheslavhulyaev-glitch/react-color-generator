11c8e67242c32442453233ab35d0b3e0c1dabdad
Viacheslav Huliaiev <viacheslav.huliaiev@innocode.no>
2026-02-26 09:12:24 +0100

    Added copy to clipboard

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,20 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ color, index }) => {
     const { hex, weight } = color;
+    const saveToClipboard = async() => {
+        if (navigator.clipboard) {
+            await navigator.clipboard.writeText(`#${hex}`);
+            toast.success("Copied to clipboard");
+        } else {
+            toast.error("Clipboard not supported");
+        }
+    }
   return (
-    <article className={`color ${index > 10 && "color-light"}`} style={{ background: `#${hex}` }}>
+    <article className={`color ${index > 10 && "color-light"}`} 
+        style={{ background: `#${hex}` }}
+        onClick={saveToClipboard}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
     </article>
